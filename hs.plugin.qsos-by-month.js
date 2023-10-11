b80a71e100150fdfdeaea53c8a20f037893561e5
Thomas Cort <linuxgeek@gmail.com>
2023-10-11 05:23:00 -0400

    feat(plugins): time series plugins

---
+++ hs.plugin.qsos-by-month.js
@@ -0,0 +1,67 @@
+'use strict';
+
+class QsoByMonthHsPlugin extends HsPlugin {
+    constructor() {
+        super('QSOs by Month');
+    }
+
+    init(adif_file) {
+        this.months = new Map();
+    }
+
+    #tally(map, key) {
+        const oldCount = map.get(key);
+        const newCount = (oldCount ?? 0) + 1;
+        map.set(key, newCount);
+    }
+
+    processQso(qso) {
+        const timestamp = moment(this.createTimestamp(qso.QSO_DATE, qso.TIME_ON)).utc();
+
+        this.#tally(this.months, timestamp.format('MM'));
+    }
+
+    render() {
+
+        const months = [...this.months.entries()].sort((a, b) => {
+            const [ a_month, a_count ] = a;
+            const [ b_month, b_count ] = b;
+            return parseInt(a_month) - parseInt(b_month);
+        });
+
+        const monthThead = document.createElement('thead');
+        monthThead.appendChild(this.createTaggedText('th', 'Month of Year'));
+        monthThead.appendChild(this.createTaggedText('th', 'Count'));
+
+        const monthTbody = document.createElement('tbody');
+
+        [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'].forEach((month, index) => {
+
+            const result = months.find(entry => entry[0] === `${index + 1}`.padStart(2, '0'));
+
+            const tr = document.createElement('tr');
+            tr.appendChild(this.createTaggedText('td', month));
+            tr.appendChild(this.createTaggedText('td', result ? result[1] : '0'));
+            monthTbody.appendChild(tr);
+        });
+
+        const monthTable = document.createElement('table');
+        monthTable.appendChild(monthThead);
+        monthTable.appendChild(monthTbody);
+
+        const monthCard = document.createElement('div');
+        monthCard.classList.add('card');
+        monthCard.appendChild(monthTable);
+
+        const monthSection = this.createSection('QSOs by Month of Year', monthCard);
+
+        const results = document.getElementById('results');
+        results.appendChild(monthSection);
+    }
+
+    exit() {
+        this.months = new Map();
+    }
+}
+
+hs_plugin_register(QsoByMonthHsPlugin);
---
+++ hs.plugin.qsos-by-year.js
@@ -0,0 +1,67 @@
+'use strict';
+
+class QsoByYearHsPlugin extends HsPlugin {
+    constructor() {
+        super('QSOs by Year');
+    }
+
+    init(adif_file) {
+        this.years = new Map();
+    }
+
+    #tally(map, key) {
+        const oldCount = map.get(key);
+        const newCount = (oldCount ?? 0) + 1;
+        map.set(key, newCount);
+    }
+
+    processQso(qso) {
+        const timestamp = moment(this.createTimestamp(qso.QSO_DATE, qso.TIME_ON)).utc();
+
+        this.#tally(this.years, timestamp.format('YYYY'));
+    }
+
+    render() {
+
+        const years = [...this.years.entries()].sort((a, b) => {
+            const [ a_year, a_count ] = a;
+            const [ b_year, b_count ] = b;
+            return parseInt(a_year) - parseInt(b_year);
+        });
+
+        const yearThead = document.createElement('thead');
+        yearThead.appendChild(this.createTaggedText('th', 'Year'));
+        yearThead.appendChild(this.createTaggedText('th', 'Count'));
+
+        const yearTbody = document.createElement('tbody');
+
+        for (let year = parseInt(years[0][0]); years.length > 0 && year <= parseInt(years[years.length - 1][0]); year += 1) {
+            const result = years.find(entry => entry[0] === `${year}`);
+
+            const tr = document.createElement('tr');
+            tr.appendChild(this.createTaggedText('td', `${year}`));
+            tr.appendChild(this.createTaggedText('td', result ? result[1] : '0'));
+            yearTbody.appendChild(tr);
+        }
+
+        const yearTable = document.createElement('table');
+        yearTable.appendChild(yearThead);
+        yearTable.appendChild(yearTbody);
+
+        const yearCard = document.createElement('div');
+        yearCard.classList.add('card');
+        yearCard.appendChild(yearTable);
+
+        const yearSection = this.createSection('QSOs by Year', yearCard);
+
+        const results = document.getElementById('results');
+        results.appendChild(yearSection);
+
+    }
+
+    exit() {
+        this.years = new Map();
+    }
+}
+
+hs_plugin_register(QsoByYearHsPlugin);
