c642a4613a4e351dd51cb821bc8d8bf21d6959bc
Thomas Cort <linuxgeek@gmail.com>
2023-10-27 05:45:08 -0400

    feat(plugins): add awards and dxcc plugins

---
+++ hs.plugin.awards.js
@@ -0,0 +1,61 @@
+'use strict';
+
+class AwardsHsPlugin extends HsPlugin {
+    constructor() {
+        super('Awards Tracking');
+    }
+
+    init(adif_file) {
+        this.labels = ['DXCC Entities', 'Gridsquares',  'ITU Zones',    'CQ Zones', 'Prefixes', 'Callsigns'];
+        this.fields = ['DXCC',          'GRIDSQUARE',   'ITUZ',         'CQZ',      'PFX',      'CALL'];
+        this.values = new Array(this.fields.length).fill(0).map(value => new Set());
+    }
+
+    processQso(qso) {
+        this.fields.forEach((field, i) => {
+            if (typeof qso[field] === 'string' && qso[field].length > 0) {
+                this.values[i].add(qso[field]);
+            }
+        });
+    }
+
+    render() {
+
+        const tally_thead = document.createElement('thead');
+        tally_thead.appendChild(this.createTaggedText('th', 'Category'));
+        tally_thead.appendChild(this.createTaggedText('th', 'Count'));
+
+        const tally_tbody = document.createElement('tbody');
+
+        this.fields.forEach((field, i, fields) => {
+
+            const count = [...this.values[i].keys()].length;
+
+            const tally_tr = document.createElement('tr');
+            tally_tr.appendChild(this.createTaggedText('td', this.labels[i]));
+            tally_tr.appendChild(this.createTaggedText('td', count));
+
+            tally_tbody.appendChild(tally_tr);
+        });
+
+        const tally_table = document.createElement('table');
+        tally_table.appendChild(tally_thead);
+        tally_table.appendChild(tally_tbody);
+
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.appendChild(tally_table);
+
+        const section = this.createSection('Awards Tracking', card);
+
+        const results = document.getElementById('results');
+        results.appendChild(section);
+
+    }
+
+    exit() {
+        this.categories = new Map();
+    }
+}
+
+hs_plugin_register(AwardsHsPlugin);
---
+++ hs.plugin.dxcc.js
@@ -0,0 +1,66 @@
+'use strict';
+
+class DxccHsPlugin extends HsPlugin {
+    constructor() {
+        super('DXCC');
+    }
+
+    init(adif_file) {
+        this.dxcc = new Map();
+    }
+
+    processQso(qso) {
+        if (typeof qso.DXCC === 'string' && qso.DXCC.length > 0) {
+            this.tally(this.dxcc, qso.DXCC);
+        }
+    }
+
+    render() {
+
+        const tcadif = require('tcadif');
+
+        const tally_thead = document.createElement('thead');
+        tally_thead.appendChild(this.createTaggedText('th', 'Entity'));
+        tally_thead.appendChild(this.createTaggedText('th', 'Count'));
+        tally_thead.appendChild(this.createTaggedText('th', 'Percent'));
+
+        const tally_tbody = document.createElement('tbody');
+
+        Object.entries(tcadif.enums.Dxcc).forEach(([ dxcc, label ]) => {
+
+            const count = this.dxcc.get(dxcc);
+            if (!count) {
+                return;
+            }
+
+            const percent = this.getPercent(this.dxcc, count);
+
+            const tally_tr = document.createElement('tr');
+            tally_tr.appendChild(this.createTaggedText('td', label));
+            tally_tr.appendChild(this.createTaggedText('td', count));
+            tally_tr.appendChild(this.createTaggedText('td', percent));
+
+            tally_tbody.appendChild(tally_tr);
+        });
+
+        const tally_table = document.createElement('table');
+        tally_table.appendChild(tally_thead);
+        tally_table.appendChild(tally_tbody);
+
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.appendChild(tally_table);
+
+        const section = this.createSection('DXCC', card);
+
+        const results = document.getElementById('results');
+        results.appendChild(section);
+
+    }
+
+    exit() {
+        this.dxcc = new Map();
+    }
+}
+
+hs_plugin_register(DxccHsPlugin);
